7ecdef4efa9bc6dfc91687bf981fd6451c15e1eb
Machad3x <machad3x@gmail.com>
2025-04-28 16:52:38 -0400

    dm3q: Update blobs from S918BXXU8DYD9

--- lineage_dm3q.mk
+++ lineage_dm3q.mk
@@ -16,13 +16,14 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_dm3q
 PRODUCT_DEVICE := dm3q
-PRODUCT_BRAND := Samsung
+PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-S918B
-PRODUCT_MANUFACTURER := Samsung
+PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc=$(call normalize-path-list, "dm3qxxx-user 14 UP1A.231005.007 S918BXXS7CXL2 release-keys")
-
-BUILD_FINGERPRINT := samsung/dm3qxxx/dm3q:14/UP1A.231005.007/S918BXXS7CXL2:user/release-keys
+    BuildDesc="dm3qxxx-user 15 AP3A.240905.015.A2 S918BXXU8DYD9 release-keys" \
+    BuildFingerprint=samsung/dm3qxxx/dm3q:15/AP3A.240905.015.A2/S918BXXU8DYD9:user/release-keys \
+    DeviceProduct=dm3qxxx \
+    SystemName=dm3qxxx
