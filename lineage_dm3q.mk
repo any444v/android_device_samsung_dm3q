9c1e5a9bec92ed0d16fb48facb7d633ea6eccc71
any444v <162444641+any444v@users.noreply.github.com>
2025-07-02 11:04:04 +0500

    specify some variables

--- lineage_dm3q.mk
+++ lineage_dm3q.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,17 +8,18 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from the device configuration.
+# Inherit from dm3q device
 $(call inherit-product, device/samsung/dm3q/device.mk)
 
-# Inherit from the AOSP configuration.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_dm3q
 PRODUCT_DEVICE := dm3q
+PRODUCT_MANUFACTURER := samsung
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-S918B
-PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
@@ -27,9 +28,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=samsung/dm3qxxx/dm3q:15/AP3A.240905.015.A2/S918BXXS8DYF1:user/release-keys \
     DeviceProduct=dm3qxxx \
     SystemName=dm3qxxx \
-    RisingChipset="Snapdragon 8 Gen 2" \
+    RisingChipset="Snapdragon® 8 Gen 2" \
     RisingMaintainer="3kz"
 
+# RisingOS variables
 RISING_MAINTAINER := 3kz
 
 TARGET_ENABLE_BLUR := true
